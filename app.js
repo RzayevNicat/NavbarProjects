8fd248f15d50f32af951d833200b86cd0f692ba5
Nicat Rzayev <99348762+RzayevNicat@users.noreply.github.com>
2022-08-14 17:49:34 +0400

    first commit

---
+++ app.js
@@ -0,0 +1,6 @@
+let navigation = document.querySelector(".navigation")
+let toggle = document.querySelector(".toggle")
+toggle.onclick = function() {
+    navigation.classList.toggle("active")
+    
+}
